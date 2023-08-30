545e2dc9a99c147656596cde8feb76e4ba50314c
Ridho Ray <ridhorayputra033@gmail.com>
2023-08-30 09:02:00 +0700

    chore: udpate

---
+++ LaraJqueryRay/resources/views/partials/sidebar.blade.php
@@ -0,0 +1,15 @@
+<nav class="nav-vertical">
+      <div class="nav-content">
+        <p>Home</p>
+      </div>
+      <div class="nav-content">
+        <p>
+            Our Product
+        </p>
+      </div>
+      <div class="nav-content">
+        <p>
+            About US
+        </p>
+      </div>
+    </nav>
