5efc62ffe1d0208117870fbb3e85bfe02c48304b
Ridho Ray <ridhorayputra033@gmail.com>
2023-08-30 13:35:13 +0700

    final

--- script.js
+++
@@ -1,9 +0,0 @@
-const menu = document.getElementById('menu-button')
-const sidebarc = document.getElementsByClassName('sidebar')[0];
-
-console.log('mantap')
-
-menu.addEventListener('click', function(){
-    sidebarc.classList.toggle('hide');
-console.log('ok')
-})
